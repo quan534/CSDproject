fd3d5f262d1bfd0bc65c6b9d9d29dca8ab368530
quan534 <quannmdt@gmail.com>
2026-08-05 14:08:50 +0700

    Update _main.py

--- _main.py
+++ _main.py
@@ -18,7 +18,7 @@ def main():
         6. CLIShell(tất cả components trên)
         7. shell.run()
     """
-    user_manager      = UserManager()
+    gay_manager      = UserManager()
     suggestion_engine = SuggestionEngine(user_manager)
     analytics         = NetworkAnalytics(user_manager)
     data_manager      = DataManager(user_manager)
